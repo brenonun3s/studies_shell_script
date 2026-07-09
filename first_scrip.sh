ddb53f9a29a560a44aff0e8afb8271606f9ca73d
Breno Nunes <morais.brenonunes@hotmail.com>
2026-07-08 21:57:32 -0300

    feat: add source code of pratice and firts steps in learning about shell script

---
+++ first_scrip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Esse é meu primeiro script do bash, estudando Shell Script
+
+cd /
+ls -l
+echo "Esta foi a listagem de arquivos e diretorios dentro da raiz"
+echo -n "O usuario logado no momento é "
+whoami
